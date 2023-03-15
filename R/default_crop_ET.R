3d15b1026db75a9db031877e01dfc3b05abb6d38
evawilson <evawilson@ucsb.edu>
2023-03-15 10:42:53 -0700

    initial commit

---
+++ R/default_crop_ET.R
@@ -0,0 +1,10 @@
+#' default_crop_ET
+#'
+#' A data frame that stores crop type and respective ET values
+#'
+#' @format a data frame that contains a column for each crop type (wheat, corn, barley) and one observation:
+#' \itemize{
+#' \item ET values
+#' }
+#'
+"default_crop_ET"
---
+++ R/precipitation.R
@@ -0,0 +1,8 @@
+#' Precipitation data for irrigation scheduling
+#'
+#' A random distribution of precipitation data
+#'
+#' @format A string with 50 precipitation values where mean is 2.5 mm and sd is 1 mm. This is generated using random distribution.
+#'
+"precipitation"
+
